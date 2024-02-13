6ae14089e28c8c323a6ca22291736e8f66276872
Anton Gadimba <antonhck@gmail.com>
2024-02-13 22:12:23 +0200

    First init

---
+++ wp-content/plugins/wpseo-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ec073197612c9987bc91aafd3296766a8cbf746e',
+        'name' => 'yoast/wpseo-woocommerce',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'dev_requirement' => false,
+        ),
+        'yoast/wpseo-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ec073197612c9987bc91aafd3296766a8cbf746e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
